20a39f5c3643df09588be8e7f5a801770314e060
avikkk19 <avinashkamadri14@gmail.com>
2025-03-31 17:56:21 +0530

    fixed line under use casees

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -227,7 +227,7 @@ const HeroSection = () => {
         </div> */}
 
         {/* Open Source Section */}
-        <div className="bg-transparent backdrop-blur-lg text-white py-20 border-t border-zinc-900">
+        <div className="bg-transparent backdrop-blur-lg text-white py-20 ">
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="text-center mb-12">
               <h2 className="text-5xl font-bold mb-4">Proudly Open Source</h2>
